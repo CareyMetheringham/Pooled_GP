af0750085c5f40dc96b7c335ffa553dd44b10db5
Carey Metheringham <c.l.metheringham@qmul.ac.uk>
2018-11-30 13:04:57 +0000

    Read in existing fix & gt tables Need to add tests and ability to read from vcf

--- R/read_in_ind_data.R
+++ R/read_in_ind_data.R
@@ -1,3 +1,42 @@
+#' Read in the fix table
+#' @param wd working directory containing fix.table
+#'
+#' @return an 8 col data frame:
+#' SNP CROM POS ID REF ALT QUAL FILTER
+#' @export
+#'
+#' @examples
+#' read_fix_table("./extdata")
+read_fix_table <- function(wd){
+  fix <- paste(wd, "fix.table", sep = "/")
+  fix_table <- fread(fix)
+  snp_positions <- paste(fix_table$CHROM, fix_table$POS, sep = "_")
+  fix_table2 <- data.frame(snp_positions, fix_table)
+  colnames(fix_table2) <-
+    c("SNP", "CROM", "POS", "ID", "REF", "ALT", "QUAL", "FILTER")
+  return(fix_table2)
+}
+
+#' Read in the genotype table - need to fix warnings
+#' @param wd working directory containing genotype.table
+#'
+#' @return a data frame with named rows
+#' @export
+#'
+#' @examples
+#' read_gt_table("./extdata")
+read_gt_table <- function(wd){
+  genotype <- paste(wd, "genotype.table", sep = "/")
+  genotype_table <- fread(genotype, sep = "\t")
+  snps_in_ind <- genotype_table$V1
+  gt <- as.data.frame(genotype_table[, -1])
+  rownames(gt) <- snps_in_ind
+  return(gt)
+}
+
+
+
+
 #read in vcf data
 
 #read in genotype table
