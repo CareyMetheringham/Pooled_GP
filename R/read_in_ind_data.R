990dbc72b97bae7b2ff9828552e4294b2f8fabc1
Carey Metheringham <c.l.metheringham@qmul.ac.uk>
2018-12-28 16:54:14 +0000

    apply column names correctly

--- R/read_in_ind_data.R
+++ R/read_in_ind_data.R
@@ -56,24 +56,21 @@ read_vcf_file <- function(vcf_file){
   ind_vcf <- read.vcfR(file=vcf_file, limit = 1e+07, cols = NULL,
                        convertNA = TRUE, checkFile = TRUE, check_keys = TRUE, verbose = TRUE)
   ind_fix <- as.data.frame(getFIX(ind_vcf))
-  ind_fix <- rbind(paste(ind_fix$CHROM, ind_fix$POS, sep="_"), ind_fix)
+
   colnames(ind_fix) <-
-    c("SNP", "CROM", "POS", "ID", "REF", "ALT", "QUAL", "FILTER")
-
+    c("CROM", "POS", "ID", "REF", "ALT", "QUAL", "FILTER")
+  ind_fix <- rbind(paste(ind_fix$CHROM, ind_fix$POS, sep="_"), ind_fix)
+  colnames(ind_fix)[1] <- "SNP"
   write.table(ind_fix,"fix.table",sep="\t",quote = FALSE, row.names = FALSE)
-
   ind_gt <- extract.gt(ind_vcf, element = "GT", mask = FALSE, as.numeric = TRUE,
                        return.alleles = FALSE, IDtoRowNames = TRUE, extract = TRUE,
                        convertNA = FALSE)
-
   get_na <- extract.gt(ind_vcf, element = "GT", mask = FALSE, as.numeric = FALSE,
                        return.alleles = FALSE, IDtoRowNames = TRUE, extract = TRUE,
                        convertNA = FALSE)
 
   #replace 0 with NA where relivant
   ind_gt[get_na=="./."] <- NA
-
   write.table(ind_gt,"genotype.table",sep="\t",quote = FALSE)
-
   return(list(gt = ind_gt, fix = ind_fix))
 }
