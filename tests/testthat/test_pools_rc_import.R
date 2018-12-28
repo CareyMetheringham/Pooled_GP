4f687867ce9d89740a440483561c75c55404df97
Carey Metheringham <c.l.metheringham@qmul.ac.uk>
2018-12-28 14:53:48 +0000

    Need to fix test functions

--- tests/testthat/test_pools_rc_import.R
+++ tests/testthat/test_pools_rc_import.R
@@ -8,26 +8,27 @@ pool_info <- "../../extdata/example_pop_data.csv"
 gwas <- "../../extdata/example_100_hits.gwas"
 
 test_that("Pools rc data object as expected", {
-  expect_named(read_in_pools_rc(find_pools_rc(pool_dir), pool_info, gwas, 10),
+  expect_named(read_in_pools_rc(fread(find_pools_rc(pool_dir)), fread(pool_info), gwas, 10),
                c("y", "prov", "maa", "mia", "snp_id", "major", "minor"))
 })
 
 test_that("gt is a matrix", {
-  expect_that(read_in_pools_rc(find_pools_rc(pool_dir),
-                               pool_info, gwas, 10)$mia,
+  expect_that(read_in_pools_rc(fread(find_pools_rc(pool_dir)),
+                               fread(pool_info), fread(gwas), 10)$mia,
               is_a("matrix"))
-  expect_that(read_in_pools_rc(find_pools_rc(pool_dir),
-                               pool_info, gwas, 10)$maa,
+  expect_that(read_in_pools_rc(fread(find_pools_rc(pool_dir)),
+                               fread(pool_info), fread(gwas), 10)$maa,
               is_a("matrix"))
 })
 
 test_that("y is an integer", {
-  expect_that(read_in_pools_rc(find_pools_rc(pool_dir), pool_info, gwas, 10)$y,
+  expect_that(read_in_pools_rc(fread(find_pools_rc(pool_dir)),
+              fread(pool_info), fread(gwas), 10)$y,
               is_a("integer"))
 })
 
 test_that("Length of y equals length of prov", {
-  data <- read_in_pools_rc(find_pools_rc(pool_dir), pool_info, gwas, 10)
+  data <- read_in_pools_rc(fread(find_pools_rc(pool_dir)), fread(pool_info), fread(gwas), 10)
   expect_length(data$y, length(data$prov))
 })
 
