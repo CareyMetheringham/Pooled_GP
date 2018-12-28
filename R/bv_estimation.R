5678c5fe5304248aef02758da68cc4a38cc00692
Carey Metheringham <c.l.metheringham@qmul.ac.uk>
2018-12-28 17:38:54 +0000

    try better version of get_ebv

--- R/bv_estimation.R
+++ R/bv_estimation.R
@@ -2,14 +2,22 @@
 
 #need estimated effect sizes for loci and gt matrix for test population
 
+# get_ebv <- function(ees_table, gt_matrix){
+#   num_ind <- ncol(gt_matrix)
+#   alt_matrix <- 2 - gt_matrix
+#   ebv <- list()
+#   for (i in 1:num_ind){
+#     effect_mia <- ees_table$EES.MIA * gt_matrix[, i]
+#     effect_maa <- ees_table$EES.MAA * alt_matrix[, i]
+#     ebv[[i]] <- sum(effect_maa, effect_mia)
+#   }
+#   return(unlist(ebv))
+# }
+
 get_ebv <- function(ees_table, gt_matrix){
-  num_ind <- ncol(gt_matrix)
   alt_matrix <- 2 - gt_matrix
-  ebv <- list()
-  for (i in 1:num_ind){
-    effect_mia <- ees_table$EES.MIA * gt_matrix[, i]
-    effect_maa <- ees_table$EES.MAA * alt_matrix[, i]
-    ebv[[i]] <- sum(effect_maa, effect_mia)
-  }
-  return(unlist(ebv))
+  effect_mia <-  t(gt_matrix) %*% ees_table$EES.MIA
+  effect_maa <-  t(alt_matrix) %*% ees_table$EES.MAA
+  ebv <- sum(effect_mia, effect_maa)
+  return(ebv)
 }
