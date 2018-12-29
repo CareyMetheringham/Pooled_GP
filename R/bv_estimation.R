cd144a110dd5a70340762eb1da6998c15d47e0a8
Carey Metheringham <c.l.metheringham@qmul.ac.uk>
2018-12-29 16:08:23 +0000

    Tidy new version of get_ebv and document

--- R/bv_estimation.R
+++ R/bv_estimation.R
@@ -1,23 +1,18 @@
 #functions to estimate breeding values in a test population
 
-#need estimated effect sizes for loci and gt matrix for test population
-
-# get_ebv <- function(ees_table, gt_matrix){
-#   num_ind <- ncol(gt_matrix)
-#   alt_matrix <- 2 - gt_matrix
-#   ebv <- list()
-#   for (i in 1:num_ind){
-#     effect_mia <- ees_table$EES.MIA * gt_matrix[, i]
-#     effect_maa <- ees_table$EES.MAA * alt_matrix[, i]
-#     ebv[[i]] <- sum(effect_maa, effect_mia)
-#   }
-#   return(unlist(ebv))
-# }
-
+#' Calculate EBV from GT and ES
+#'
+#' @param ees_table data table containing results of rrblup
+#' @param gt_matrix matrix of 0, 1, 2 and NA
+#'
+#' @return ebv vector
+#' @export
+#'
+#' @examples
 get_ebv <- function(ees_table, gt_matrix){
   alt_matrix <- 2 - gt_matrix
   effect_mia <-  t(gt_matrix) %*% ees_table$EES.MIA
   effect_maa <-  t(alt_matrix) %*% ees_table$EES.MAA
   ebv <- effect_mia + effect_maa
-  return(ebv)
+  return(as.vector(ebv))
 }
