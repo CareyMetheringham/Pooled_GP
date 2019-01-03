0f1dfe10d11a213b354b3c91064fd780a98050b2
Carey Metheringham <c.l.metheringham@qmul.ac.uk>
2019-01-03 10:24:16 +0000

    Fixed test functions Need to improve/gernalise test of maa + mia = 1

--- tests/testthat/test_pools_rc_import.R
+++ tests/testthat/test_pools_rc_import.R
@@ -4,47 +4,47 @@ library(data.table)
 
 #Example files to use in tests
 pool_dir <- "../../extdata/Pools_RC"
-pool_info <- fread("../../extdata/example_pop_data.csv")
-gwas <- fread("../../extdata/example_100_hits.gwas")
+pool_info <- "../../extdata/example_pop_data.csv"
+gwas <- "../../extdata/example_100_hits.gwas"
 
 test_that("Pools rc data object as expected", {
-  expect_named(read_in_pools_rc(fread(find_pools_rc(pool_dir)), fread(pool_info), gwas, 10),
+  expect_named(read_in_pools_rc(find_pools_rc(pool_dir), fread(pool_info), gwas, 10),
                c("y", "prov", "maa", "mia", "snp_id", "major", "minor"))
 })
 
 test_that("gt is a matrix", {
-  expect_that(read_in_pools_rc(fread(find_pools_rc(pool_dir)),
-                               fread(pool_info), fread(gwas), 10)$mia,
+  expect_that(read_in_pools_rc(find_pools_rc(pool_dir),
+                               fread(pool_info), gwas, 10)$mia,
               is_a("matrix"))
-  expect_that(read_in_pools_rc(fread(find_pools_rc(pool_dir)),
-                               fread(pool_info), fread(gwas), 10)$maa,
+  expect_that(read_in_pools_rc(find_pools_rc(pool_dir),
+                               fread(pool_info), gwas, 10)$maa,
               is_a("matrix"))
 })
 
 test_that("y is an integer", {
-  expect_that(read_in_pools_rc(fread(find_pools_rc(pool_dir)),
-              fread(pool_info), fread(gwas), 10)$y,
+  expect_that(read_in_pools_rc(find_pools_rc(pool_dir),
+              fread(pool_info), gwas, 10)$y,
               is_a("integer"))
 })
 
 test_that("Length of y equals length of prov", {
-  data <- read_in_pools_rc(fread(find_pools_rc(pool_dir)), fread(pool_info), fread(gwas), 10)
+  data <- read_in_pools_rc(find_pools_rc(pool_dir), fread(pool_info), gwas, 10)
   expect_length(data$y, length(data$prov))
 })
 
 test_that("Nrows in gt matrix = number of snps", {
- data <- read_in_pools_rc(find_pools_rc(pool_dir), pool_info, gwas, 10)
+ data <- read_in_pools_rc(find_pools_rc(pool_dir), fread(pool_info), gwas, 10)
   expect_length(data$snp_id, nrow(data$mia))
   expect_length(data$snp_id, nrow(data$maa))
 })
 
 test_that("Nrows in gt matrix = length y", {
-  data <- read_in_pools_rc(find_pools_rc(pool_dir), pool_info, gwas, 10)
+  data <- read_in_pools_rc(find_pools_rc(pool_dir), fread(pool_info), gwas, 10)
   expect_length(data$y, ncol(data$maa))
   expect_length(data$y, ncol(data$mia))
 })
 
 test_that("maa freq + mia freq = 1", {
-  data <- read_in_pools_rc(find_pools_rc(pool_dir), pool_info, gwas, 10)
-  expect_equal((data$maa + data$mia), 1)
+  data <- read_in_pools_rc(find_pools_rc(pool_dir), fread(pool_info), gwas, 10)
+  expect_equal((data$maa[1,1] + data$mia[1,1]), 1)
 })
