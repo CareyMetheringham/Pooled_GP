d44f0c5efbfe9b9ce5801bf3ffe3638b1ca41160
Carey Metheringham <c.l.metheringham@qmul.ac.uk>
2019-01-07 11:19:45 +0000

    likelihood function output effect sizes do not make sense - needs more work

--- R/mcmc.R
+++ R/mcmc.R
@@ -7,8 +7,8 @@
 #' @export
 #'
 #' @examples
-#' data <- read_in_pools_rc("./extdata/test.pool_rc", fread("./extdata/test.pool_info"), "./extdata/test.gwas", 10)
-#' get_parameters(data, 0.1)
+#' test_data <- read_in_pools_rc("./extdata/test.pool_rc", fread("./extdata/test.pool_info"), "./extdata/test.gwas", 10)
+#' get_parameters(test_data, 0.1)
 #' sim_data <- produce_sim_data(10, 100, 100)
 #' get_parameters(sim_data, 0.1)
 get_parameters <- function(simData, effect) {
@@ -32,7 +32,7 @@ get_parameters <- function(simData, effect) {
       combVar = combVar,
       exclMean = exclMean,
       exclVar = exclVar,
-      obsFreq = p
+      obsFreq = as.vector(p)
     )
   )
 }
@@ -72,9 +72,61 @@ get_exp_var <- function(p, effect, exp_mean){
 run_mcmc <- function(my_data, effect, chain = 10^3){
   for (i in 1:chain){
     params <- get_parameters(my_data, effect)
-    l <- sample(1:length(my_data$snp_id))
+    l <- sample(1:length(my_data$snp_id), 1)
+    e <- -1000:1000 / 200
+    f <- params$obsFreq[[l]]
+    m <- params$exclMean[[l]]
+    var_g <- params$exclVar[[l]]
+    var_e <- calc_var_e(var_g)
+    sd_combined <- sqrt(var_g + var_e)
+    p_high <- calc_p_high(f, m, sd_combined, e)
+    obs_high <- get_obs_high(my_data)[l]
+    #stopgap use of whole numbers for likelihood function
+    num_total <- round(f * 1000)
+    num_high <- round(obs_high * 1000)
+    likelihood <- dbinom(num_high, num_total, p_high)
+    #diffuse prior?
+    prior <- rbeta(length(p_high), 1, 1)
+    #calculate posterior
+    post <- prior * likelihood
+    x <- which.max(post)
+    effect[l] <- e[x]
   }
+  return(effect)
+}
 
+#need to be able to vary cutoff value??
+calc_p_high <- function(f, m, sd_comb, e){
+  p_high <- f * pnorm(m + 2 * e, 0.5, sd_comb) + (1 - f) * pnorm(m + e, 0.5, sd_comb)
+  return(p_high)
+}
+
+#should this be different??? wrong equation
+calc_p_low <- function(f, m, sd_comb, e){
+  p_high <- f * pnorm(m + 2 * e, 0.5, sd_comb) + (1 - f) * pnorm(m + e, 0.5, sd_comb)
+  return(p_high)
+}
+
+get_obs_high <- function(my_data, high = '2'){
+  mia_high <- my_data$mia[, my_data$y == high]
+  mean_high <- rowMeans(mia_high)
+  return(mean_high)
+}
+
+#' Calculate Enviromental Varience on Assumed h2
+#'
+#' @param var_g
+#' @param h2
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' calc_var_e(0.1)
+#' calc_var_e(0.1, 1) # should give 0 write test
+calc_var_e <- function(var_g, h2 = 0.3){
+  var_e <- (var_g * ( 1 - h2 )) / h2
+  return(var_e)
 }
 
 
