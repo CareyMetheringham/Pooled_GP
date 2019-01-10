129cf18eb61a8e6f9b58cbfd5bc5589a93865edb
Carey Metheringham <c.l.metheringham@qmul.ac.uk>
2019-01-10 10:24:40 +0000

    changed ees_table to ees to avoid confusion with other terms

--- R/bv_estimation.R
+++ R/bv_estimation.R
@@ -2,19 +2,19 @@
 
 #' Calculate EBV from GT and ES
 #'
-#' @param ees_table data table containing results of rrblup
+#' @param ees data table containing results of rrblup
 #' @param gt_matrix matrix of 0, 1, 2 and NA
 #'
 #' @return ebv vector
 #' @export
 #'
 #' @examples
-get_ebv <- function(ees_table, gt_matrix){
+get_ebv <- function(ees, gt_matrix){
   gt_matrix <- gt_matrix[order(rownames(gt_matrix)),]
-  ees_table <- ees_table[order(ees_table$SNP), ]
+  ees <- ees[order(ees$SNP), ]
   alt_matrix <- 2 - gt_matrix
-  effect_mia <-  t(gt_matrix) %*% ees_table$EES.MIA
-  effect_maa <-  t(alt_matrix) %*% ees_table$EES.MAA
+  effect_mia <-  t(gt_matrix) %*% ees$EES.MIA
+  effect_maa <-  t(alt_matrix) %*% ees$EES.MAA
   ebv <- effect_mia + effect_maa
   return(as.vector(ebv))
 }
