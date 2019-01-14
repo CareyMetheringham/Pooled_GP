cd5009c62a535a3d36b7fdd767fbe2eac3f0f3cf
Carey Metheringham <c.l.metheringham@qmul.ac.uk>
2019-01-14 16:19:26 +0000

    test varience calcs

--- tests/testthat/test_mcmc.R
+++ tests/testthat/test_mcmc.R
@@ -1,3 +1,8 @@
 context("Test MCMC Functions")
 library(gppool)
 library(data.table)
+
+test_that("Varience Calcs Correct",{
+  expect_equal(calc_var_e(0.1, 1), 0)
+  expect_equal(calc_var_e(0, 0.5), 0)
+})
