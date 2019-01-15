4408e6bf6cfcc55d036f7f7d0a2fd3a2fad9e7fd
Carey Metheringham <c.l.metheringham@qmul.ac.uk>
2019-01-15 12:17:04 +0000

    issue of duplicate rows

--- R/match_training_and_test.R
+++ R/match_training_and_test.R
@@ -43,11 +43,13 @@ order_by_ees <- function(ees_table){
 #' ind_gt <- read_gt_table("./extdata/test.gt")
 #' test_data <- read_in_pools_rc("./extdata/test.pool_rc", fread("./extdata/test.pool_info"), "./extdata/test.gwas", 10)
 #' fix_allele_mismatch(fread("./extdata/test.ees_table"), ind_gt, ind_fix, test_data)
-fix_allele_mismatch <- function(ees_table, gt_table, fix_table, pop_data){
-  ees_and_maa <- data.frame(ees_table, pop_data$major) #need to ensure order is correct
+fix_allele_mismatch <- function(ees_table, gt, fix, pop_data){
+  ees_and_maa <- data.table(ees_table, pop_data$major) #need to ensure order is correct
   colnames(ees_and_maa)[6] <- "MAJOR"
-  match_by_snp <- merge(ees_and_maa, fix_table, by = "SNP") # <- This table is mostly NA!! - issue from fix?
-  corrected_gt <- correct_gt(gt_table, match_by_snp)
+  print(ees_and_maa)
+  match_snps <- merge(ees_and_maa, fix, by = "SNP") # <- This line pruduces duplicates
+  print(match_snps)
+  corrected_gt <- correct_gt(gt, match_snps)
   return(corrected_gt)
 }
 
@@ -64,19 +66,19 @@ correct_gt <- function(gt, match_snps){
   for (i in 1:nrow(gt)){
     my_row <- rownames(gt)[i]
     if (my_row %in% match_snps$SNP){
-      match_line <- match_snps[match_snps$SNP == my_row, ]
-      line <- gt[i, ]
-      if (match_line$MAJOR != match_line$REF){
+      fix_line <- match_snps[match_snps$SNP == my_row, ]
+      gt_line <- gt[i, ]
+      if (fix_line$MAJOR[1] != fix_line$REF[1]){
         for (j in 1:ncol(gt)){
           if ( gt[i, j] == 0){
-            line[j] <- 2
+            gt_line[j] <- 2
           }
           if ( gt[i, j] == 2){
-            line[j] <- 0
+            gt_line[j] <- 0
           }
         }
       }
-      swapped_gt <- rbind(swapped_gt, line)
+      swapped_gt <- rbind(swapped_gt, gt_line)
     }
   else{next}
   }
@@ -132,14 +134,21 @@ get_gt_subset <- function(snp_list, gt){
 #' test_data <- read_in_pools_rc("./extdata/test.pool_rc", fread("./extdata/test.pool_info"), "./extdata/test.gwas", 10)
 #' match_and_subset(fread("./extdata/test.ees_table"), ind_gt, ind_fix, test_data, 5)
 match_and_subset <- function(ees_table, gt, fix, pool_data, subset_size){
+  #match_snps <- match_snps_in_ind(ees_table, corrected_mismatch)
+  match_snps <- match_snps_in_ind(ees_table, gt)
+  #subset_snps <- get_ees_subset(match_snps, subset_size)
+  subset_ees <- get_ees_subset(match_snps, subset_size)
+  #gt_subset <- get_gt_subset(subset_snps$SNP, corrected_mismatch)
+  gt_subset <- get_gt_subset(subset_ees$SNP, gt)
   corrected_mismatch <-
-    fix_allele_mismatch(ees_table, gt, fix, pool_data)  #<- THIS GIVES ERROR: rows empty - tested function works
-  #print(head(corrected_mismatch))
-  match_snps <- match_snps_in_ind(ees_table, corrected_mismatch)
-  #print(head(match_snps))
-  subset_snps <- get_ees_subset(match_snps, subset_size)
+    fix_allele_mismatch(subset_ees, gt_subset, fix, pool_data)
+
+  #corrected_mismatch <-
+    #fix_allele_mismatch(ees_table, gt, fix, pool_data)  #<- THIS GIVES ERROR: rows empty - tested function works
+
+
   #print(head(subset_snps))
-  gt_subset <- get_gt_subset(subset_snps$SNP, corrected_mismatch)
+
   return(list(gt = gt_subset,
-              ees = subset_snps))
+              ees = subset_ees))
 }
